729043d6ed54f6b7e37a9818b12d3dc1b0eb5e19
Amir Grozki <amirgrozki@gmail.com>
2013-09-07 20:30:07 +0300

    Initial work on personal blog.

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -1,26 +1,13 @@
 /*! Plugin options and other jQuery stuff */
 
-// dl-menu options
-$(function() {
-  $( '#dl-menu' ).dlmenu({
-    animationClasses : { classin : 'dl-animate-in', classout : 'dl-animate-out' }
-  });
-});
-
 // FitVids options
-$(function() {
+$(function($) {
+  'use strict';
+  
   $("article").fitVids();
-});
-
-$(".close-menu").click(function () {
-  $(".menu").toggleClass("disabled");
-  $(".links").toggleClass("enabled");
-});
-
-$(".about").click(function () {
-  $("#about").css('display','block');
-});
-
-$(".close-about").click(function () {
-  $("#about").css('display','');
+  
+  setTimeout(function() {
+    $('.nav-menuwrapper').addClass('nav-animate-in nav-menuwrapper-active');
+  }, 1000);
+  
 });
